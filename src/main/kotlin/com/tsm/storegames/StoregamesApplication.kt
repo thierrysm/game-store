762f5d53cfa183d898431335bc6ebc4b7b74cf45
thierry <thierrydev48@gmail.com>
2024-08-29 16:24:15 -0300

    first commit

---
+++ src/main/kotlin/com/tsm/storegames/StoregamesApplication.kt
@@ -0,0 +1,11 @@
+package com.tsm.storegames
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StoregamesApplication
+
+fun main(args: Array<String>) {
+	runApplication<StoregamesApplication>(*args)
+}
---
+++ src/test/kotlin/com/tsm/storegames/StoregamesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tsm.storegames
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StoregamesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
